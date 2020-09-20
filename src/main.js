93c2f8fa105a2a0869595e028d3c611aa39b5480
Yaroslav Yudinskikh <yaskih@mail.ru>
2020-09-20 12:20:15 +0300

    [add] firebase

--- src/main.js
+++ src/main.js
@@ -7,10 +7,23 @@ import 'element-ui/lib/theme-chalk/index.css';
 import locale from 'element-ui/lib/locale/lang/ru-RU';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import * as firebase from 'firebase';
 
 Vue.use(ElementUI, { locale });
 
 Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBdbNAkWESgdz_Zv9-qM3usvQKp1nvS9fY',
+  authDomain: 'digiathero---med.firebaseapp.com',
+  databaseURL: 'https://digiathero---med.firebaseio.com',
+  projectId: 'digiathero---med',
+  storageBucket: 'digiathero---med.appspot.com',
+  messagingSenderId: '518940199497',
+  appId: '1:518940199497:web:850f0e5ffed9e5333b0d98',
+});
+
+export const db = firebase.app().database();
 
 new Vue({
   router,
